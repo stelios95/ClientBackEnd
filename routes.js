e1462c1212d11b5a60796040413180fc3b0f2db0
Asterios Ntinos <ntinosasterios@gmail.com>
2020-10-03 19:27:22 +0300

    [FEAT] implemented single term search

--- routes.js
+++ routes.js
@@ -2,11 +2,26 @@ const express = require("express");
 const searchRoutes = express.Router();
 const cors = require("cors");
 const app = express();
+const Site = require("./siteSchema");
 
 app.use(cors());
 
-searchRoutes.route("/search").get((req, res) => {
-  res.status(200).send("OK");
+searchRoutes.route("/search").post((req, res) => {
+  getSearchResults(req.body.searchTerm, res);
 });
+
+async function getSearchResults(searchTerm, res) {
+  try {
+    console.log("inside");
+    const results = await Site.find(
+      { $text: { $search: searchTerm } },
+      { score: { $meta: "textScore" }, loc: 1, title: 1, _id: 0 }
+    ).sort({ score: { $meta: "textScore" } });
+    res.status(200).send(results);
+  } catch (err) {
+    console.log(JSON.stringify(err));
+    res.status(400).send(err);
+  }
+}
 
 module.exports = searchRoutes;
---
+++ siteSchema.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const Schema = mongoose.Schema;
+
+//defing the site document schema
+const pageSchema = new Schema({
+  title: {
+    type: String,
+    //required: true,
+    //unique: true
+  },
+  processedTitle: {
+    type: String,
+  },
+  loc: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  method: {
+    type: Number,
+    required: true,
+  },
+  lastmod: {
+    type: Date,
+  },
+  changefreq: {
+    type: String,
+  },
+  priority: {
+    type: String,
+  },
+  isNews: {
+    type: Boolean,
+  },
+});
+
+pageSchema.plugin(uniqueValidator);
+module.exports = mongoose.model("Site", pageSchema);
